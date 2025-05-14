deca589323e50076cad8033c7ce0419c9e6003e7
AreLory <arena.lorenzo2406@gmail.com>
2025-05-14 10:25:09 +0200

    Delete dist directory

--- dist/functions.js
+++
@@ -1,6 +0,0 @@
-export function verificaCriteriCittadino(cittadino, incentivo) {
-    return incentivo.criteriEleggibilta.every(criterio => { var _a; return (_a = criterio.verificaCittadino) === null || _a === void 0 ? void 0 : _a.call(criterio, cittadino); });
-}
-export function verificaCriteriStartup(startup, incentivo) {
-    return incentivo.criteriEleggibilta.every(criterio => { var _a; return (_a = criterio.verificaStartup) === null || _a === void 0 ? void 0 : _a.call(criterio, startup); });
-}
